c46b66381846281da4c04011535c105875388f0d
blacklistcpp <72122797+blacklistcpp@users.noreply.github.com>
2021-03-07 00:33:47 +0300

    Add files via upload

---
+++ Source/common/tables.h
@@ -0,0 +1,17 @@
+typedef struct SPlayerTable
+{
+
+	int		aiPremiumTimes[PREMIUM_MAX_NUM];
+
+
+// ARATILIR 
+
+
+
+// ALTINA EKLEN?R
+#ifdef ENABLE_DUNGEON_JOIN_CORDS
+	long	d_map_index;
+#endif
+
+
+
